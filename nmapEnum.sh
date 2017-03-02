63eaf08876a6eacb5f3fa29a1fde9c4ee3e5d80e
root <root@localhost.localdomain>
2017-03-02 05:13:09 -0500

    Nmap Enumeration Script

---
+++ nmapEnum.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOUR='\033[0m'
+
+if [ $# -ne 1 ]; then
+    printf "\n${RED}[+]Nmap Enumeration Script\n"
+    printf "${GREEN}[+]Usage nmapEnum.sh <target>\n\n"
+    exit
+fi
+
+dirName='./nmapEnum-'$1
+
+if [ ! -d $dirName ]; then
+	mkdir $dirName
+fi
+
+printf "\n${RED}[+]Running version scan...${NOCOLOUR}\n"
+nmap -sV --version-all -p- -oX $dirName/version-scan-xml.xml $1 | tee -a $dirName/version-scan.txt
+ 
+#tcp
+printf "\n${RED}[+]Running TCP scan...${NOCOLOUR}\n"
+nmap -Pn -A -sC -sS -T 4 -p- $1 | tee -a $dirName/tcp-scan.txt
+ 
+#aggressive/service
+printf "\n${RED}[+]Running aggressive service scan...${NOCOLOUR}\n"
+nmap -A $1 | nmap -sC $1 | tee -a $dirName/aggressive-service.txt
+ 
+#udp
+printf "\n${RED}[+]Running UDP scan...${NOCOLOUR}\n"
+nmap -Pn -A -sC -sU -T 4 --top-ports 500 $1 | tee -a $dirName/udp-500.txt
+ 
+#SearchSploit
+searchsploit -w --nmap $dirName/version-scan-xml.xml | tee -a $dirName/SearchSploit.txt
