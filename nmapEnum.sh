8ff9edc9d4dfc62f69ce1bdf9d63526c1981a043
root <root@localhost.localdomain>
2017-03-07 23:42:52 -0500

    Syn Scan Added

--- nmapEnum.sh
+++ nmapEnum.sh
@@ -15,6 +15,9 @@ if [ ! -d $dirName ]; then
 	mkdir $dirName
 fi
 
+printf "\n${RED}[+]Running SYN scan...${NOCOLOUR}\n"
+nmap -sS -p- $1 | tee -a $dirName/Syn-scan.txt
+
 printf "\n${RED}[+]Running version scan...${NOCOLOUR}\n"
 nmap -sV --version-all -p- -oX $dirName/version-scan-xml.xml $1 | tee -a $dirName/version-scan.txt
  
@@ -28,7 +31,8 @@ nmap -A $1 | nmap -sC $1 | tee -a $dirName/aggressive-service.txt
  
 #udp
 printf "\n${RED}[+]Running UDP scan...${NOCOLOUR}\n"
-nmap -Pn -A -sC -sU -T 4 --top-ports 500 $1 | tee -a $dirName/udp-500.txt
- 
+nmap -Pn -A -sC -sU -T 4 --top-ports 100 $1 | tee -a $dirName/udp-100.txt
+
 #SearchSploit
+printf "\n${RED}[+]Running SearchSploit...${NOCOLOUR}\n"
 searchsploit -w --nmap $dirName/version-scan-xml.xml | tee -a $dirName/SearchSploit.txt
